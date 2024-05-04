b5e6f5057ee547a0afa2ac9023f3052fd6ed36b3
ALAIN ELOMA <alainelomapro@gmail.com>
2024-05-04 17:41:15 +0100

    pfp not required anymore

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: {type: String, required: true },
-  photo: { type: String, required: true },
+  photo: { type: String, required: false },
 })
 
 const User = models.User || model('User', UserSchema);
