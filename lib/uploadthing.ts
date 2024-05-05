fba07dc0ba2a918c9afd398be3992246e17a5fd2
ALAIN ELOMA <alainelomapro@gmail.com>
2024-05-05 10:04:58 +0100

    middleware matcher slithly changed

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,5 +1,9 @@
-import { generateReactHelpers } from "@uploadthing/react/hooks";
- 
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
- 
-export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
+import {
+    generateUploadButton,
+    generateUploadDropzone,
+  } from "@uploadthing/react";
+   
+  import type { OurFileRouter } from "@/app/api/uploadthing/core";
+   
+  export const UploadButton = generateUploadButton<OurFileRouter>();
+  export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   publicRoutes: [
@@ -16,6 +16,6 @@ export default authMiddleware({
 });
  
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
  
