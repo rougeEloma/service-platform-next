158e4fa73a26407c6dfbd2fa722ac13bf00183b7
ALAIN ELOMA <alainelomapro@gmail.com>
2024-05-09 04:24:33 +0100

    middleware - update

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export default authMiddleware({
   publicRoutes: [
     '/',
     '/events/:id',
-    // '/api/webhook/clerk',
+    '/api/webhooks/clerk',
     '/api/webhook/stripe',
     '/api/uploadthing'
   ],
